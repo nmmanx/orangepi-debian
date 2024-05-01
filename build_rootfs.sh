17825bde75f917fd9ef8ba22ae22a36017cb8800
Man Nguyen <nmman37@gmail.com>
2024-05-01 14:13:00 +0700

    Merged scripts into a single build_rootfs.sh

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -1,32 +1,83 @@
 #!/bin/bash
 
-MOUNTPOINT=/orangepi-debian
-SKIP_DEBOOTSTRAP=0
+SKIP_DEBOOTSTRAP_DEFAULT=1
 
-if [ ! -d $MOUNTPOINT ]; then
-    echo "Please run this script inside a container with this dir mounted to $MOUNTPOINT"
-    exit 1
-fi
+_ischroot="$(ischroot; test "$?" -eq "1"; echo $?)"
+IS_2ND_STATE="$_ischroot"
+
+MOUNTPOINT=/orangepi-debian
 
 OUTDIR=${MOUNTPOINT}/out
 ROOTFS=${OUTDIR}/rootfs
 
 mkdir -p $OUTDIR $ROOTFS
 
-echo "root" | sudo -S  echo "Auto gain root permission"
+if [ "$IS_2ND_STATE" == "0" ]; then
+    echo "########## 1st Stage ##########"
 
-if [ "$SKIP_DEBOOTSTRAP" != "1" ]; then
-    echo "debootstrap 1st stage..."
-    sudo debootstrap --arch=arm64 --foreign --variant=minbase --verbose buster $ROOTFS
-else
-    echo "Skip debootstrap 1st stage"
+    SKIP_DEBOOTSTRAP=$SKIP_DEBOOTSTRAP_DEFAULT
+
+    if [ ! -d $MOUNTPOINT ]; then
+        echo "Please run this script inside a container with this dir mounted to $MOUNTPOINT"
+        exit 1
+    fi
+    
+    echo "root" | sudo -S  echo "Auto gain root permission"
+
+    if [ "$SKIP_DEBOOTSTRAP" != "1" ]; then
+        echo "debootstrap 1st stage..."
+        sudo debootstrap --arch=arm64 --foreign --variant=minbase --verbose buster $ROOTFS
+    else
+        echo "Skip debootstrap 1st stage"
+    fi
+
+    sudo cp -v /usr/bin/qemu-aarch64-static ${ROOTFS}/usr/bin/
+    echo "nameserver 8.8.8.8" | sudo tee ${ROOTFS}/etc/resolv.conf
+    echo "nameserver 8.8.4.4" | sudo tee -a ${ROOTFS}/etc/resolv.conf
+
+    echo "Finished 1st stage"
+
+    # Start 2nd state
+    sudo cp -v ./build_rootfs.sh ${ROOTFS}/run/
+    sudo chroot $ROOTFS bash -c "export SKIP_DEBOOTSTRAP=$SKIP_DEBOOTSTRAP && /run/build_rootfs.sh"
+
+    echo "Done!"
+    exit 0
 fi
 
-sudo cp -v /usr/bin/qemu-aarch64-static ${ROOTFS}/usr/bin/
-echo "nameserver 8.8.8.8" | sudo tee ${ROOTFS}/etc/resolv.conf
-echo "nameserver 8.8.4.4" | sudo tee -a ${ROOTFS}/etc/resolv.conf
+# The below script will run on the new rootfs as root user
+echo "########## 2nd Stage ##########"
 
-sudo cp -v ./run_on_rootfs.sh ${ROOTFS}/run/
-sudo chroot $ROOTFS bash -c "export SKIP_DEBOOTSTRAP=$SKIP_DEBOOTSTRAP && /run/run_on_rootfs.sh"
+USERNAME=orangepi
+PASSWORD=root
+HOSTNAME=orangepi4lts
 
-echo "Done"
+export LANG=C
+
+if [ "$SKIP_DEBOOTSTRAP" != "1" ]; then
+    echo "debootstrap 2nd stage... (could be long, please wait)"
+    /debootstrap/debootstrap --keep-debootstrap-dir --verbose --second-stage
+else
+    echo "Skip debootstrap 2nd stage"
+fi
+
+# Require --cap-add=CAP_SYS_ADMIN passed to docker run
+mount -t proc none /proc
+mount -t sysfs sysfs /sys
+
+apt update
+apt install -y sudo
+
+# Init admin user
+echo "Adding user"
+useradd -m -s /bin/bash -G sudo -u 1000 -p $PASSWORD $USERNAME
+
+# Hostname
+echo $HOSTNAME > /etc/hostname
+echo "Hostname: $(cat /etc/hostname)"
+
+# Install additional packages
+echo "Installing additional packages..."
+apt install -y vim net-tools ethtool udev wireless-tools wpasupplicant
+
+echo "Finished 2nd stage"
