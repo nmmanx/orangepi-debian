c1fff0ac10b961c255b84360c2fcb22cba2b1758
Man Nguyen <nmman37@gmail.com>
2024-05-03 18:43:42 +0700

    Extract compiler archive only once

--- toolchain.sh
+++ toolchain.sh
@@ -8,6 +8,8 @@ GCC_NAME=gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu
 GCC_LINK=https://snapshots.linaro.org/gnu-toolchain/13.0-2022.11-1/aarch64-linux-gnu/gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu.tar.xz
 GCC_ARCHIVE_FILE=$TOOLCHAIN_DIR/$(basename ${GCC_LINK})
 
+export CROSS_COMPILE_PREFIX=${TOOLCHAIN_DIR}/${GCC_NAME}/bin/aarch64-linux-gnu-
+
 prepare_toolchain () {
     mkdir -p $TOOLCHAIN_DIR
 
@@ -17,13 +19,17 @@ prepare_toolchain () {
     fi
 
     echo "Downloaded: $GCC_ARCHIVE_FILE"
-    echo "Extracting..."
-    #tar -xf $GCC_ARCHIVE_FILE -C $TOOLCHAIN_DIR
+    
+    if [ ! -f "${CROSS_COMPILE_PREFIX}gcc" ]; then
+        echo "Extracting..."
+        tar -xf $GCC_ARCHIVE_FILE -C $TOOLCHAIN_DIR
+    fi
+
+    echo "Extracted to: $TOOLCHAIN_DIR"
 }
 
 prepare_toolchain
 
-export CROSS_COMPILE_PREFIX=${TOOLCHAIN_DIR}/${GCC_NAME}/bin/aarch64-linux-gnu-
 echo CROSS_COMPILE_PREFIX=$CROSS_COMPILE_PREFIX
 
 test -f ${CROSS_COMPILE_PREFIX}gcc
