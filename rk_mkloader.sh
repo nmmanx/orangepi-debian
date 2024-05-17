652104a9bc662aa57ad5f68a149010f59572d5c0
Man Nguyen <nmman37@gmail.com>
2024-05-17 21:18:25 +0700

    Append RK miniloader to idbloader.img

--- rk_mkloader.sh
+++ rk_mkloader.sh
@@ -9,11 +9,18 @@ if [ ! -d "./configs/$TARGET" ]; then
 fi
 source configs/$TARGET/config
 
+require_config CONFIG_ROCKCHIP_PLATFORM
+require_config CONFIG_ROCKCHIP_DDR_BIN
+require_config CONFIG_ROCKCHIP_MINILOADER
+require_config CONFIG_ROCKCHIP_TRUST_INI
+require_config CONFIG_UBOOT_TEXT_BASE
+
 mkdir -p $LOADER_OUT
 
 log "Making idbloader.img..."
 cd $RKBIN_DIR && tools/mkimage -n $CONFIG_ROCKCHIP_PLATFORM \
     -T rksd -d $CONFIG_ROCKCHIP_DDR_BIN $LOADER_OUT/idbloader.img
+cat $CONFIG_ROCKCHIP_MINILOADER >> $LOADER_OUT/idbloader.img
 
 log "Making uboot_rk.img..."
 cd $RKBIN_DIR && tools/loaderimage --pack --uboot $UBOOT_OUT/u-boot.bin \
@@ -28,9 +35,9 @@ mv $RKBIN_DIR/trust.img $LOADER_OUT/trust.img
 log "Making loaders.img..."
 
 rm -f $LOADER_OUT/loaders.img
-dd if=$LOADER_OUT/idbloader.img of=$LOADER_OUT/loaders.img conv=notrunc
-dd if=$LOADER_OUT/uboot_rk.img of=$LOADER_OUT/loaders.img seek=$(( 16384 - 64 )) conv=notrunc
-dd if=$LOADER_OUT/trust.img of=$LOADER_OUT/loaders.img seek=$(( 24576 - 64 )) conv=notrunc
+dd if=$LOADER_OUT/idbloader.img of=$LOADER_OUT/loaders.img bs=$CONFIG_SD_BLOCK_SIZE conv=notrunc
+dd if=$LOADER_OUT/uboot_rk.img of=$LOADER_OUT/loaders.img bs=$CONFIG_SD_BLOCK_SIZE seek=$(( 16384 - 64 )) conv=notrunc
+dd if=$LOADER_OUT/trust.img of=$LOADER_OUT/loaders.img bs=$CONFIG_SD_BLOCK_SIZE seek=$(( 24576 - 64 )) conv=notrunc
 
 log "Written to $LOADER_OUT/loaders.img"
 
