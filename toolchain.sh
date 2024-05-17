8900bb898e270f9dfc445edd9cfe69b290e9cf9e
Man Nguyen <nmman37@gmail.com>
2024-05-17 21:20:58 +0700

    toolchain.sh: switch to ARM gcc

--- toolchain.sh
+++ toolchain.sh
@@ -4,11 +4,18 @@ source configs/common
 
 TOOLCHAIN_DIR=$OUTDIR/toolchain
 
-GCC_NAME=gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu
-GCC_LINK=https://snapshots.linaro.org/gnu-toolchain/13.0-2022.11-1/aarch64-linux-gnu/gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu.tar.xz
+# Linaro GCC
+# GCC_NAME=gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu
+# GCC_LINK=https://snapshots.linaro.org/gnu-toolchain/13.0-2022.11-1/aarch64-linux-gnu/gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu.tar.xz
+
+# ARM GCC
+GCC_NAME=arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu
+GCC_LINK=https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+
 GCC_ARCHIVE_FILE=$TOOLCHAIN_DIR/$(basename ${GCC_LINK})
 
-export CROSS_COMPILE_PREFIX=${TOOLCHAIN_DIR}/${GCC_NAME}/bin/aarch64-linux-gnu-
+# export CROSS_COMPILE_PREFIX=${TOOLCHAIN_DIR}/${GCC_NAME}/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_PREFIX=${TOOLCHAIN_DIR}/${GCC_NAME}/bin/aarch64-none-linux-gnu-
 
 prepare_toolchain () {
     mkdir -p $TOOLCHAIN_DIR
